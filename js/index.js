faa09e438d74cd262e439836f34400efcbe70265
Mark Lynch <malynch7@gmail.com>
2021-01-29 15:56:53 -0500

    restructured index.css, angled section boundaries, reworked mobile sidenav

--- js/index.js
+++ js/index.js
@@ -1,27 +1,38 @@
+/*
+Description: Primary front-end logic for portfolio at malynch7.github.io.
+Author: Mark Lynch
+Created: 10-07-2020
+*/
+
+/*==== General ==============================================================*/
+
 $(document).ready(function(){
     $('.parallax').parallax();
     $('.scrollspy').scrollSpy();
     $('.sidenav').sidenav();
     $('.tabs').tabs();
 
-
+    const infoView = $("#info-view");
     $('#main-nav').pushpin({
-        top: $("#info-view").offset().top,
-        bottom: $("#info-view").offset().top + $("#info-view").outerHeight()
+        top: infoView.offset().top,
+        bottom: infoView.offset().top + infoView.outerHeight()
             - $("#main-nav").height()
     });
     //initProjectView();
 });
 
+
+/*==== Featured Projects ====================================================*/
+
 function initProjectView(){
-    let cards = document.querySelectorAll('#project-view .card');
+    let cards = $('#project-view .card');
     let maxHeight = 0;
 
-    for (let index in cards){
-        maxHeight = Math.max(cards[index].style.height, maxHeight);
+    for (const card of cards){
+        maxHeight = Math.max(parseInt(card.style.height), maxHeight);
     }
-    for (let index in cards){
-        cards[index].height(maxHeight);
+    for (const card of cards){
+        card.height(maxHeight);
     }
 }
 
